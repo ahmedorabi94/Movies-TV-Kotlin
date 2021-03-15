b11104f7a57e844b1b5e838914ed491cdedb848d
Ahmed Orabi <eng.ahmedorabi94@gmail.com>
2021-03-16 00:54:28 +0200

    update TVShowViewModel.kt

--- app/src/main/java/com/example/galleryapp/viewmodels/TVShowViewModel.kt
+++ app/src/main/java/com/example/galleryapp/viewmodels/TVShowViewModel.kt
@@ -1,14 +1,51 @@
 package com.example.galleryapp.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.example.galleryapp.data.entity.MovieResponse
 import com.example.galleryapp.data.repo.TVRepository
 import com.example.galleryapp.utils.Resource
 import kotlinx.coroutines.Dispatchers
 
 class TVShowViewModel @ViewModelInject constructor(private val repo: TVRepository) : ViewModel() {
 
+    private val _movieResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieTopRatedResponse: LiveData<Resource<MovieResponse>> get() = _movieResponse
+
+    private val _moviePopularResponse = MutableLiveData<Resource<MovieResponse>>()
+    val moviePopularResponse: LiveData<Resource<MovieResponse>> get() = _moviePopularResponse
+
+    private val _movieAiringTodayResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieAiringTodayResponse: LiveData<Resource<MovieResponse>> get() = _movieAiringTodayResponse
+
+    private val _movieOnTheAirResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieOnTheAirResponse: LiveData<Resource<MovieResponse>> get() = _movieOnTheAirResponse
+
+    private val _movieActionResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieActionResponse: LiveData<Resource<MovieResponse>> get() = _movieActionResponse
+
+    private val _movieTopCrimeResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieTopCrimeResponse: LiveData<Resource<MovieResponse>> get() = _movieTopCrimeResponse
+
+    private val _movieTopComedyResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieTopComedyResponse: LiveData<Resource<MovieResponse>> get() = _movieTopComedyResponse
+
+    private val _movieTopWarResponse = MutableLiveData<Resource<MovieResponse>>()
+    val movieTopWarResponse: LiveData<Resource<MovieResponse>> get() = _movieTopWarResponse
+
+    init {
+        getTopWarTv()
+        getTopComedyTv()
+        getTopCrimeTv()
+        getTopActionTv()
+        getOnTheAirTVSHOWS()
+        getAiringTodayTVSHOWS()
+        getPopularTVSHOWS()
+        getTopRatedTVSHOWS()
+    }
 
     fun getTopRatedTVSHOWS() = liveData(Dispatchers.IO) {
 
